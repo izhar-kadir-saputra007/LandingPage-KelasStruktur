5a9df5cad4f472309a4bcb49c15705c79c851177
Izhar Kadir Saputra <izharalians@gmail.com>
2026-03-11 02:35:04 +0800

    add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,8 +13,37 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://landing-page-kelas-struktur.vercel.app"),
+
   title: "Jago Desain Struktur Gedung - Kelas Struktur",
-  description: "Kelas online untuk belajar desain struktur gedung sesuai SNI dan tahan gempa. Materi lengkap, praktis, dan terstruktur untuk mahasiswa, fresh graduate, freelancer, dan pemula.",
+  description:
+    "Kelas online untuk belajar desain struktur gedung sesuai SNI dan tahan gempa. Materi lengkap, praktis, dan terstruktur untuk mahasiswa, fresh graduate, freelancer, dan pemula.",
+
+  openGraph: {
+    title: "Jago Desain Struktur Gedung - Kelas Struktur",
+    description:
+      "Belajar desain struktur gedung sesuai SNI & tahan gempa dengan materi terstruktur dan praktis.",
+    url: "https://landing-page-kelas-struktur.vercel.app/",
+    siteName: "Kelas Struktur",
+    images: [
+      {
+        url: "/og-kelas-struktur.jpg", // otomatis jadi full URL karena metadataBase
+        width: 1200,
+        height: 630,
+        alt: "Kelas Struktur - Desain Struktur Gedung",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Jago Desain Struktur Gedung - Kelas Struktur",
+    description:
+      "Belajar desain struktur gedung sesuai SNI & tahan gempa dengan materi terstruktur dan praktis.",
+    images: ["/og-kelas-struktur.jpg"],
+  },
 };
 
 export default function RootLayout({
@@ -23,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="id">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
